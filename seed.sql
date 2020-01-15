056a2cd62d27a2cc3334a01849f6e89c9e7fa667
Bill Murray <buckibill@gmail.com>
2020-01-14 19:43:41 -0800

    added database file and piucs

---
+++ seed.sql
@@ -0,0 +1,146 @@
+
+
+INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Frying Pan', 'fryingpan.jpg', 'cookware', 'fry food', 10, 12.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Sautee Pan', 'sautepan.jpg', 'cookware', 'sautee food', 10, 7.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Pressure Cooker', 'pressurecooker.jpg', 'cookware', 'pressure cook food', 10, 24.69);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Sauce Pan', 'saucepan.jpg', 'cookware', 'make sauce', 108.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Cookware Set', 'cookwareset.jpg', 'cookware', 'cookware set to cook', 10, 54.79);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Tea Kettle', 'teakettle.jpg', 'cookware', 'make tea', 10, 11.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Stock Pot', 'stockpot.jpg', 'cookware', 'to make stew', 10, 14.79);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Steamer', 'steamer.jpg', 'cookware', 'steam vegetable to perfection', 10, 18.59);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Spoons', 'spoon.jpg', 'kitchen_utensils', 'to eat food', 10, 1);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Forks', 'fork.jpg', 'kitchen_utensils', 'to eat food', 10, 1);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Knives', 'knife.jpg', 'kitchen_utensils', 'to eat food', 10, 1);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Cleaver', 'cleaver.jpg', 'kitchen_utensils', 'to cut food into little tiny pieces', 10, 6.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Knife Block', 'knifeblock.jpg', 'kitchen_utensils', 'to cut things', 10, 10.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Cutting Board', 'cuttingboard.jpg', 'kitchen_utensils', 'to cut on', 10, 6.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Cheese Knife', 'cheeseknife.jpg', 'kitchen_utensils', 'to cut the cheese', 10, 4.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Blender', 'blender.jpg', 'small_appliances', 'to blend', 10, 13.69);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Juicer', 'juicer.jpg', 'small_appliances', 'to make juice', 10, 24.79);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Food Processor', 'processor.jpg', 'small_appliances', 'to process food', 10, 27.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Mixer', 'mixer.jpg', 'small_appliances', 'to mix food', 10, 17.39);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Toaster', 'toaster.jpg', 'small_appliances', 'to toast bread', 10, 14.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Microwave', 'microwave.jpg', 'small_appliances', 'quick and easy cooking', 10, 35.79);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Rice Cooker', 'ricecooker.jpg', 'small_appliances', 'to cook rice', 10, 19.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Dinner Plate', 'dinnerplate.jpg', 'tableware', 'to eat on', 10, 2);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Salad Plate', 'saladplate.jpg', 'tableware', 'to eat salad on', 10, 2);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Bowl', 'bowl.jpg', 'tableware', 'to eat from', 10, 2);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Coffee Cup', 'coffeecup.jpg', 'tableware', 'to drink coffee from', 10, 2);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Glass', 'glass.jpg', 'tableware', 'to drink from', 10, 2);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Apron', 'apron.jpg', 'misc', 'so you dont get food allover yourself', 10, 16.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Pizza Cutter', 'pizzacutter.jpg', 'misc', 'to cut pizza with', 10, 7.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Gloves', 'gloves.jpg', 'misc', 'because your messy', 10, 11.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Stainless Steel Kitchen Scale', 'scale.jpg', 'misc', 'to weigh food', 10, 15.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('5 Shelf Storage Unit', 'storageunit.jpg', 'misc', 'to store food items', 10, 26.99);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Trash Can', 'trashcan.jpg', 'misc', 'eliminate waste', 10, 24.69);
+    INSERT INTO kitchenItems
+    (name, picurl, category, description, quantity, itemPrice)
+VALUES
+    ('Silverware Tray Organizer', 'silverwareorganizer.jpg', 'misc', 'to organize your silverware', 10, 12.69);
+
+
+    
+
+
+
+    
+    
